60e5bae33708d2499324efde7efe795ee31b37cb
Shinya Kamiaka <shinya@kamiaka.net>
2021-08-02 10:56:12 +0900

    Fix event notification message

--- src/background.ts
+++ src/background.ts
@@ -153,7 +153,7 @@ async function notifyEvent(ev: ScheduleEvent, url?: string, duration?: number) {
     {
       title,
       message:
-        (duration ? `${duration} ${__('minutes_before')}` : '') + ev.notes,
+        (duration ? `${duration} ${__('minutes_before')}: ` : '') + ev.notes,
     },
     {
       onClicked: () => {
